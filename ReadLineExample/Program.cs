4bb05b1709d5aaca0da304847125bce49d634997
Editting <deltagare@LEXJONKAF33>
2020-09-08 08:53:12 +0200

    Add project files.

---
+++ ReadLineExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReadLineExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Name:");
+            String usrName = Console.ReadLine();
+            Console.Write("Age:");
+            int usrAge = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Gender (M/F):");
+            char usrGender = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Name:" + usrName);
+            Console.WriteLine("Age:" + usrAge);
+            Console.WriteLine("Gender:" + usrGender);
+        }
+    }
+}
